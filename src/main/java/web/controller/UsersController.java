777c6cbb22d38923e4dadcd8e51a6e307fa2b511
Виталий Корнилов <ratlion@rambler.ru>
2022-07-13 02:19:30 +0300

    First commit

---
+++ src/main/java/web/controller/UsersController.java
@@ -0,0 +1,28 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import web.model.User;
+import web.service.UserService;
+
+import java.util.List;
+
+@Controller
+public class UsersController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/")
+    public String printUsers(Model model) {
+        List<User> users = userService.getUsersList();
+        model.addAttribute("users", users);
+        return "index";
+    }
+}
---
+++ src/main/java/web/dao/UserDaoImp.java
@@ -0,0 +1,32 @@
+package web.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public UserDaoImp(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        List<User> users = new ArrayList<>();
+        for (long a = 1; a < 10; a++) {
+            users.add(new User(a, "Name " + a, "lastName " + a));
+        }
+        //TypedQuery<User> query = entityManager.createQuery("FROM User", User.class);
+        return users;
+    }
+}
